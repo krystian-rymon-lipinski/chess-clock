a9104b91cb735c06834f4bff4c7c9d8b1985887e
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2024-01-04 17:39:06 +0100

    Migrate to androidX

--- app/src/main/java/com/krystian/chessclock/customMatchPackage/CustomMatchDialogFragment.java
+++ app/src/main/java/com/krystian/chessclock/customMatchPackage/CustomMatchDialogFragment.java
@@ -5,16 +5,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.krystian.chessclock.ExtraValues;
-import com.krystian.chessclock.R;
+import com.krystianrymonlipinski.chessclock.R;
 
 public class CustomMatchDialogFragment extends DialogFragment implements SeekBar.OnSeekBarChangeListener {
 
