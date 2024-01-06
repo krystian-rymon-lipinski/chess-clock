9b2e5d2d1abb214f0760318ffd6f91eebd430f08
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2024-01-06 21:10:53 +0100

    Migrate database classes to Kotlin

--- app/src/main/java/com/krystian/chessclock/customMatchPackage/CustomMatchDatabaseHelper.kt
+++ app/src/main/java/com/krystian/chessclock/customMatchPackage/CustomMatchDatabaseHelper.kt
@@ -1,30 +1,28 @@
-package com.krystian.chessclock.customMatchPackage;
+package com.krystian.chessclock.customMatchPackage
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
-public class CustomMatchDatabaseHelper extends SQLiteOpenHelper {
-
-    private final static String DB_NAME = "CUSTOM_MATCH_DATABASE";
-    private final static int DB_VERSION = 1;
-
-    public CustomMatchDatabaseHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
+class CustomMatchDatabaseHelper(context: Context?) :
+    SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE " + CustomMatchDatabase.CUSTOM_MATCHES_TABLE +
+                    "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    CustomMatchDatabase.NAME + " TEXT, " +
+                    CustomMatchDatabase.NUMBER_OF_GAMES + " INTEGER);"
+        )
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + CustomMatchDatabase.CUSTOM_MATCHES_TABLE +
-                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                CustomMatchDatabase.NAME + " TEXT, " +
-                CustomMatchDatabase.NUMBER_OF_GAMES + " INTEGER);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if(DB_VERSION < 1) {
-            onCreate(db);
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        if (DB_VERSION < 1) {
+            onCreate(db)
         }
+    }
+
+    companion object {
+        private const val DB_NAME = "CUSTOM_MATCH_DATABASE"
+        private const val DB_VERSION = 1
     }
 }
