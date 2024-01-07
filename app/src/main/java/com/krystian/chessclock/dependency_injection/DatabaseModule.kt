3a04d15ef1fdeb5f7162b292c9aff638005ffb32
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2024-01-07 02:55:31 +0100

    Add Database class and its hilt model

---
+++ app/src/main/java/com/krystian/chessclock/dependency_injection/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.krystian.chessclock.dependency_injection
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.krystian.chessclock.room.ChessClockDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideRoomDatabase(@ApplicationContext context: Context) : RoomDatabase {
+        return Room.databaseBuilder(
+            context,
+            ChessClockDatabase::class.java,
+            "chess-clock-database"
+        ).build()
+    }
+}
---
+++ app/src/main/java/com/krystian/chessclock/room/ChessClockDatabase.kt
@@ -0,0 +1,8 @@
+package com.krystian.chessclock.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(version = 1)
+abstract class ChessClockDatabase : RoomDatabase() {
+}
