4c5149964feccc5044897887234f69ffbfb74843
Krystian Rymon Lipinski <krystian.r.lipinski@gmail.com>
2024-01-12 13:48:47 +0100

    Change IDs in database from ints to longs

--- app/src/main/java/com/krystian/chessclock/model/CustomGame.kt
+++ app/src/main/java/com/krystian/chessclock/model/CustomGame.kt
@@ -1,10 +1,11 @@
 package com.krystian.chessclock.model
 
 data class CustomGame(
-    val id: Int = 0,
+    val id: Long = 0,
     val whiteTime: Int,
     val whiteIncrement: Int,
     val blackTime: Int,
     val blackIncrement: Int,
-    val matchId: Int
+    val matchId: Long
 )
+
--- app/src/main/java/com/krystian/chessclock/room/CustomMatchEntity.kt
+++ app/src/main/java/com/krystian/chessclock/room/CustomMatchEntity.kt
@@ -6,6 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = ChessClockDatabase.customMatchTable)
 data class CustomMatchEntity(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = ChessClockDatabase.customMatchTableIdColumn) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = ChessClockDatabase.customMatchTableIdColumn) val id: Long = 0,
     val name: String
 )
